2b8dfe5d1f9489e76aeb928beeee08ce96b23142
grojas <grojas@dwsoftware.mx>
2017-02-09 16:47:01 -0600

    index.tss modified and index.js commented We've commented code inside index.js cause had troubles when we try to add two labels

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,16 +1,17 @@
- $.botonAbrirVentana.addEventListener('click', function(_evento) {
-     //  alert('Boton pinchado');
-     // abrir el controlador detalle
+//// lineas comentadas para evitar conflictos cuando se comenta el boton
+// $.botonAbrirVentana.addEventListener('click', function(_evento) {
+//     //  alert('Boton pinchado');
+//     // abrir el controlador detalle
+//
+//     // Crear una instancia del controlador detalle
+//     var detalle = Alloy.createController('detalle', 'Rojas');
+//
+//     // Acceder a la vista asociada al controlador detalle
+//     detalle = detalle.getView(); // detalle.xml
+//
+//     // Abrir esa vista
+//     detalle.open();
+//
+// });
 
-     // Crear una instancia del controlador detalle
-     var detalle = Alloy.createController('detalle', 'Rojas');
-
-     // Acceder a la vista asociada al controlador detalle
-     detalle = detalle.getView(); // detalle.xml
-
-     // Abrir esa vista
-     detalle.open();
-
- });
-
- $.index.open();
+$.index.open();
