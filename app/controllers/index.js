7af7b6ec550beeed5754c2da75a8465d21a643e2
grojas <grojas@dwsoftware.mx>
2017-02-10 15:27:06 -0600

    Using LisView We've created new elements such as: controller, view and tss called listView to do some test using listView

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,17 +1,25 @@
-//// lineas comentadas para evitar conflictos cuando se comenta el boton
-// $.botonAbrirVentana.addEventListener('click', function(_evento) {
-//     //  alert('Boton pinchado');
-//     // abrir el controlador detalle
-//
-//     // Crear una instancia del controlador detalle
-//     var detalle = Alloy.createController('detalle', 'Rojas');
-//
-//     // Acceder a la vista asociada al controlador detalle
-//     detalle = detalle.getView(); // detalle.xml
-//
-//     // Abrir esa vista
-//     detalle.open();
-//
-// });
+function llamarTelefono(e) {
+    console.log(JSON.stringify(e, null, 5));
+
+    // Capturando el valor de la propiedad title
+    var telefono = e.source.title;
+
+    if (Ti.Platform.canOpenURL) {
+        // Llamar por telefono
+        Ti.Platform.openURL('tel://' + telefono);
+    } else {
+        alert('El dispositivo no puede realizar llamadas..');
+    }
+}
+
+$.telefono.addEventListener('click', function(e) {
+    llamarTelefono(e);
+});
+
+$.btnAbrirLV.addEventListener('click', function(e) {
+    var listView = Alloy.createController('listView');
+    listView = listView.getView();
+    listView.open();
+});
 
 $.index.open();
---
+++ app/controllers/listView.js
@@ -0,0 +1,7 @@
+// // Arguments passed into this controller can be accessed via the `$.args` object directly or:
+// var args = $.args;
+
+
+$.lView.addEventListener('itemclick', function(e) {
+    console.log(JSON.stringify(e, null, 4));
+});
